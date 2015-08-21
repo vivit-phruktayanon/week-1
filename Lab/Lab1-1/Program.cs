f4a4b2f62f898ac9555675b0911ffa7a20cefb85
vivit123 <vivitphruktayanon@gamil.com>
2015-08-21 16:39:14 +0700

    commid all lab 1

--- Lab/Lab1-1/Program.cs
+++ Lab/Lab1-1/Program.cs
@@ -11,25 +11,26 @@ namespace Lab1_1
         static void Main(string[] args)
         {
             bool flag = true;
-
+            int x = 0;
             // input
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
 
             // process
-            while (flag)
+            //while (flag)
+            //{
+            //flag = false;
+            for (int i = 0; i != input.Length - 1; i++)
             {
-                flag = false;
-                for (int i = 0; i != input.Length - 1; i++)
+                if (input[i] < input[i + 1])
                 {
-                    if (input[i] > input[i + 1])
-                    {
-                        int temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
-                        flag = true;
-                    }
+                    int temp = input[i];
+                    input[i] = input[i + 1];
+                    input[i + 1] = temp;
+                    //flag = true;
+                    x++;
                 }
             }
+            //}
 
             // output
             for (int i = 0; i != input.Length; i++)
@@ -37,6 +38,8 @@ namespace Lab1_1
                 Console.Write(input[i]);
                 Console.Write(" ");
             }
+            Console.WriteLine();
+            Console.Write(x);
             Console.ReadKey();
         }
     }
